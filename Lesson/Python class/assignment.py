3235f56df89eea00902db5c680d10d30a62854e3
Hephzisoft <hephzisoft@gmail.com>
2023-01-17 01:07:56 +0100

    made some minor changes

--- Lesson/Python class/assignment.py
+++ Lesson/Python class/assignment.py
@@ -13,3 +13,37 @@ print(f"{fahrenheit} f")
 minutes = int(input("Minutes: "))
 seconds = minutes * 60
 print(f"{seconds} sec")
+
+
+# TODO ASSIGNMENT 4
+
+string = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+print(len(string))
+print(string[::3])
+print(string.upper())
+print(string.replace('a', 'z'))
+vowels = 'aeiou'
+new_string = ''.join([c for c in string if c not in vowels])
+print(new_string)
+print(string[1::2])
+print(string + string)
+print(string[26:]+string[:26])
+print(string[-1::-2])
+print(string.translate(str.maketrans('','','az')))
+print(string[::-1])
+print(f"The string is {string}")
+
+# TODO ASSIGNMENT 5
+planet  ='earth'
+diameter= 12345
+
+print(f"The diameter of the {planet} is {diameter} kilometer")
+
+surname = ['Ade', 'Kehinde', 'Samuel', 'Thompson', 'samod']
+append_surname=['Samod', 'Bukola','Ahmed']
+extend_surname = ['Chrsti','Ite','Hephzi']
+
+print(sorted(surname.extend(extend_surname)))
+print(sorted(surname.append(append_surname)))
+
+
--- Lesson/Python class/class.py
+++ Lesson/Python class/class.py
@@ -1,9 +1,16 @@
-name = ['emmanuel', 'samod','hephzibah','christiana']
+# name = ['emmanuel', 'samod','hephzibah','christiana']
 
-class_name= ["science", 'art', 'commercial']
+# class_name= ["science", 'art', 'commercial']
 
 
+# name.append(class_name)
 
-name.append(class_name)
+# print(name)
 
-print(name)
+
+word = [1, 2, [3, 4], [5, [100, 200, ['hello']], 23, 11], 1, 7]
+print(word[3][1][0])
+first = word[3][0]
+second = word[3][1][1]
+third = word[3][1][2][0]
+list = [first, second, third]
