8e07b5cead97883f300523b069278b360d7d797e
Hephzisoft <hephzisoft@gmail.com>
2023-02-01 15:49:28 +0100

    made some changes to the  finalclass.dart

---
+++ Lesson/Dart class/class_1/bin/finalclass.dart
@@ -0,0 +1,11 @@
+void main() {
+  final hephz =   User();
+  hephz.name = "Hephzibah";
+  hephz.id  = 1;
+}
+
+
+class User {
+  int id = 0;
+  String name = '';
+}
--- Lesson/Dart class/class_1/bin/unknown.dart
+++ Lesson/Dart class/class_1/bin/unknown.dart
@@ -1,5 +1,29 @@
+import 'dart:math';
+
 void main(List<String> args) {
-  
+  print(isPrime(3));
 }
 
-int add(int a, int b) => a + b;
+bool isPrime(int number) {
+  var no = sqrt(number);
+  print(no);
+  if (number <= 1) return false;
+  for (int i = 2; i <= sqrt(number); i++) {
+    if (number % i == 0) return false;
+  }
+  return true;
+}
+
+// ignore: todo
+// TODO: write a function that will print out prime number e.g if 10 is the number it should print 1,2,3,5,7
+void checkNumber(int number) {
+  if (number <= 1) {
+    print("$number is not prime");
+  }else{
+    if (number >1){
+      for (int i=2; i < number; i++){
+        
+      }
+    }
+  }
+}
