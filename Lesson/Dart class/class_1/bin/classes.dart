91a91ccbb331fe08235f54a2d77f2e76ff656781
Hephzisoft <hephzisoft@gmail.com>
2023-02-13 11:09:29 +0100

    made some changes

--- Lesson/Dart class/class_1/bin/classes.dart
+++ Lesson/Dart class/class_1/bin/classes.dart
@@ -1,9 +1,9 @@
 // void main(List<String> args) {
-  // final obj1 = const User(
-  //    firstName: 'Ola',lastName: 'Tolu',photoUrl: 'https://www.apple.com'); //instance of a class
-  // print(obj1.name);
+//   final obj1 = const User(
+//      firstName: 'Ola',lastName: 'Tolu',photoUrl: 'https://www.apple.com'); //instance of a class
+//   print(obj1.name);
 
-  // Example obj1 = Example(public, private);
+//   Example obj1 = Example(public, private);
 // }
 
 // class User {
@@ -27,5 +27,17 @@
 //   const Example(this.public, this.private);
 
 //   /// Named paramter cannot start with underscore e.g _private
-//   const Example.namedConstructor({required this.public, required this.private});
+//   // const Example.namedConstructor({required this.public, required this.private});
 // }
+
+void main(List<String> args) {
+  final student1 = const Student( courseId: "1002",name: "Damilare",);
+  print(student1.courseId);
+}
+
+class Student {
+  final  name;
+  final  courseId;
+
+ const  Student({this.name, this.courseId});
+}
