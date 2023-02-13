c127076bad4ab9030e14f50d7f55f09a8b1dba6c
Hephzisoft <hephzisoft@gmail.com>
2023-02-13 13:54:53 +0100

    made some changes

--- Lesson/Dart class/class_1/bin/unknown.dart
+++ Lesson/Dart class/class_1/bin/unknown.dart
@@ -1,28 +1,33 @@
 import 'dart:math';
 
 void main(List<String> args) {
-  print(isPrime(3));
+  checkNumber(3);
 }
 
-bool isPrime(int number) {
-  var no = sqrt(number);
-  print(no);
-  if (number <= 1) return false;
-  for (int i = 2; i <= sqrt(number); i++) {
-    if (number % i == 0) return false;
-  }
-  return true;
-}
+// bool isPrime(int number) {
+//   var no = sqrt(number);
+//   print(no);
+//   if (number <= 1) return false;
+//   for (int i = 2; i <= sqrt(number); i++) {
+//     if (number % i == 0) return false;
+//   }
+//   return true;
+// }
 
 // ignore: todo
 // TODO: write a function that will print out prime number e.g if 10 is the number it should print 1,2,3,5,7
 void checkNumber(int number) {
+  List primeNumber = [];
   if (number <= 1) {
     print("$number is not prime");
-  }else{
-    if (number >1){
-      for (int i=2; i < number; i++){
-        
+  } else {
+    if (number > 1) {
+      for (int i = 2; i <= number; i++) {
+        if (number % i == 0) {
+          print('Not a prime $i');
+        } else {
+          print('prime number $i');
+        }
       }
     }
   }
---
+++ Lesson/Dart class/personal_research/bin/sometest.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main(List<String> args) {
+  // print(sin(45 * pi / 180));
+  // double x = 1 / sqrt(2);
+  // print((x));
+  // final hoursSinceMidnight = DateTime.now();
+  // print(hoursSinceMidnight);
+  // const myAge = 19;
+  // const friendAge = 10;
+
+  // const averageAge = ((myAge + friendAge) / 2);
+  // print(averageAge);
+
+  const myAge = 19;
+  int dogs = 3;
+  int newPuppy = ++dogs;
+  print(newPuppy);
+}
